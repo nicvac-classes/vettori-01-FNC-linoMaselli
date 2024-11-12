e86392f8f693782b5ce82aec4550d2e6de5442f7
FNC-linoMaselli <andrea.maselli@marconibari.edu.it>
2024-11-12 20:43:00 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,74 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    vector<string> V(5),W(5);
+    vector<int> X(10),Y(10);
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    V[0] = "Giuseppe Bono";
+    V[1] = "Symon c";
+    V[2] = "Lorenzo Jovanotti";
+    V[3] = "Tiziano Ferro";
+    V[4] = "Vasco Rossi";
+
+    int i = 0;
+    while(i<5){
+        cout<<"Inserire il nome del "<<i+1<<" cantante: ";
+        cin>>W[i];
+        i++;
+    }
+
+    i = 0;
+    while(i<10){
+        X[i] = i+1;
+        i++;
+    }
+
+    i = 0;
+    while(i<10){
+        Y[i] = i * 2;
+        i++; 
+    }
+
+    cout<<"1° "<<V[0]<<" 2° "<<V[1]<<" 3° "<<V[2];
+
+    i = 0;
+    while(i<5){
+        cout<< i+1 <<"° elemento di V "<<V[i]<<endl;
+        cout<< i+1 <<"° elemento di W "<<W[i]<<endl;
+        i++;
+    }
+
+    i = 0;
+    while(i<10){
+        cout<< i+1 <<"° elemento di X "<<X[i]<<endl;
+        cout<< i+1 <<"° elemento di Y "<<Y[i]<<endl;
+        i++;
+    }
+
+    i = 0;
+    while(i<10){
+        if(i%2==0){
+            cout<<i<<"° elemento pari "<<X[i]<<endl;
+        }
+        i++;
+    }
+
+    i = 0;
+    while(i<10){
+        if(i%2!=0){
+            cout<<i<<"° Elemento dispari "<<X[i]<<endl;
+        }
+        i++;
+    }
+
+    i = 0;
+    while(i<10){
+        if(X[i]%2==0){
+            cout<<X[i]<<" è pari"<<endl;
+        }
+        i++;
+    }
+    
+}
